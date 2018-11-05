df9855e3132d042e76ac84ee2d04b3606b089bd2
Elexie Munyeneh <pmunyeneh@yahoo.com>
2018-11-05 05:33:21 -0800

    Add files via upload

--- js/week1.js
+++ js/week1.js
@@ -43,8 +43,8 @@ function getArticle(userNum,pgCount,articleDiv){
 		   $(articleDiv).html(finalArticle);
 		   }
    }
-  	xhttp.open("GET", "https://elexie.github.io/groupFour/json/week1.json", true);
-//	 xhttp.open("GET", "../json/week1.json", true);
+  //	xhttp.open("GET", "https://elexie.github.io/groupFour/json/week1.json", true);
+	 xhttp.open("GET", "../json/week1.json", true);
    xhttp.send();
 }
 function slideArtcle()
@@ -85,8 +85,8 @@ function animateWidth(incomingSlide)
 
 $(document).ready(function(){
 	slideArtcle();
-  getArticle(0,4,"#munTextDiv")
+  getArticle(0,6,"#munTextDiv")
 	getArticle(1,4,"#neiTextDiv")
 	getArticle(2,5,"#varTextDiv")
-	getArticle(3,4,"#winTextDiv")
+	getArticle(3,5,"#winTextDiv")
 });
