38d48d28db2a9cace3fc1ae6b6490ced2c5bcfef
elexie <pmunyeneh@yahoo.com>
2018-11-19 15:57:33 -0800

    ..

--- js/week3.js
+++ js/week3.js
@@ -35,8 +35,8 @@ function getArticle(userNum,pgCount,articleDiv){
 		   $(articleDiv).html(finalArticle);
 		   }
    }
-	xhttp.open("GET", "https://elexie.github.io/groupFour/json/week3.json", true);
-// xhttp.open("GET", "../json/week3.json", true);
+//	xhttp.open("GET", "https://elexie.github.io/groupFour/json/week3.json", true);
+xhttp.open("GET", "../json/week3.json", true);
    xhttp.send();
 }
 function slideArtcle()
@@ -80,5 +80,5 @@ $(document).ready(function(){
   getArticle(0,6,"#munTextDiv")
 	getArticle(1,13,"#neiTextDiv")
 	getArticle(2,7,"#varTextDiv")
-	getArticle(3,4,"#winTextDiv")
+	getArticle(3,11,"#winTextDiv")
 });
