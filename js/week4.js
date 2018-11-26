126fa1ddac09686385e1ce654229ac7b86f2a30a
elexie <pmunyeneh@yahoo.com>
2018-11-25 21:36:19 -0500

    week4

--- js/week4.js
+++ js/week4.js
@@ -76,7 +76,7 @@ function animateWidth(incomingSlide)
 
 $(document).ready(function(){
 	slideArtcle();
-  getArticle(0,6,"#munTextDiv")
+  getArticle(0,5,"#munTextDiv")
 	getArticle(1,13,"#neiTextDiv")
 	getArticle(2,7,"#varTextDiv")
 	getArticle(3,11,"#winTextDiv")
